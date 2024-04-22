8614b6fc74c555970526343a09008906fbe8c279
ProjectTobi <mariiamazur2927@gmail.com>
2024-04-22 13:23:18 +0300

    Removed insecure django secret key for deployment

--- py_restaurant_service/settings.py
+++ py_restaurant_service/settings.py
@@ -22,15 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "S#perS3crEt_007",
-)
+# SECRET_KEY = os.environ.get(
+#     "SECRET_KEY",
+#     "S#perS3crEt_007",
+# )
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 # DEBUG = False
+DEBUG = "RENDER" not in os.environ
+
+
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 
